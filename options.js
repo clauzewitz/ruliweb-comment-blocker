2a1968ac06b8488b4f55baadf61104edd97e8508
Kim Sung-Jun <clauzewitz@msn.com>
2017-02-15 10:40:46 +0900

    Add files via upload

---
+++ options.js
@@ -0,0 +1,39 @@
+function saveOptions(e) {
+    if (e.defaultPrevented) {
+        return;
+    }
+
+    function onSave(result) {
+        document.querySelector("#ignore_ids").value = result.ignore_ids;
+    }
+
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    var ignoreIds = document.querySelector("#ignore_ids").value;
+    ignoreIds = ignoreIds.replace(/ /g, "");
+
+    var setting = browser.storage.local.set({
+        ignore_ids: ignoreIds
+    });
+    setting.then(onSave, onError);
+}
+
+function restoreOptions() {
+
+    function onGot(result) {
+        console.log(result, result.ignore_ids);
+        document.querySelector("#ignore_ids").value = result.ignore_ids || "";
+    }
+
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    var getting = browser.storage.local.get("ignore_ids");
+    getting.then(onGot, onError);
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
